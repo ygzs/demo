71416ce4b467c3cf5e4319b354ac7164856e23aa
ygzs <1103449238@qq.com>
2020-04-13 15:36:59 +0800

    message MVC

--- js/message.js
+++ js/message.js
@@ -1,38 +1,81 @@
-AV.init({
-  appId: "ydWA3xnYqAhAdEDHRrBcbXdQ-gzGzoHsz",
-  appKey: "FSpJbRyD0NknTLSt2KQ64B3z",
-  serverURL: "https://ydwa3xny.lc-cn-n1-shared.com"
-});
-
-let myform = document.querySelector('#List')
-myform.addEventListener('submit',function(e){
-  e.preventDefault()
-  let name = myform.querySelector('.name').value
-  let content = myform.querySelector('.content').value
-  var leancloud = AV.Object.extend('Message')
-  var message = new leancloud()
-  message.set('name',name)
-  message.set('content',content)
-  message.save()
-    .then(function (object) {
-      let li = document.createElement('li')
-      li.innerText = `${object.attributes.name}:${object.attributes.content}`
-      messageList.append(li)
-    })
-    .then( () => {
-      name = ''
-      content = ''
-    })
-})
+!function(){
+  var view = View('#List')
 
 
+  var model = {
+    initAV:function(){
+      AV.init({
+      appId: "ydWA3xnYqAhAdEDHRrBcbXdQ-gzGzoHsz",
+      appKey: "FSpJbRyD0NknTLSt2KQ64B3z",
+      serverURL: "https://ydwa3xny.lc-cn-n1-shared.com"
+      })
+    },
+    fetch:function(){
+      var query = new AV.Query('Message');
+       return query.find()
+    },
+    save:function(name,content){
+      var leancloud = AV.Object.extend('Message')
+      var message = new leancloud()
+      message.set('name',name)
+      message.set('content',content)
+      return message.save()
+    }
+  }
 
-var query = new AV.Query('Message');
-query.find().then(function (notes) {
-  let array = notes.map( (item) => item.attributes )
-  array.forEach( item => {
-    let li = document.createElement('li')
-    li.innerText = `${item.name}:${item.content}`
-    messageList.append(li)
-  })
-})
+
+  var controller = {
+    view:null,
+    model:null,
+    form:null,
+    init:function(view,model){
+      this.view = view
+      this.model = model
+      this.model.initAV()
+      this.form = View('#List')
+      this.loadMessages()
+      this.bindEvents()
+
+    },
+    loadMessages:function(){
+      this.model.fetch()
+      .then(function (notes) {
+        let array = notes.map( (item) => item.attributes )
+        array.forEach( item => {
+          let li = document.createElement('li')
+          li.innerText = `${item.name}:${item.content}`
+          messageList.append(li)
+        })
+      })
+    },
+    bindEvents:function(){
+      this.form.addEventListener('submit',(e) => {
+        e.preventDefault()
+        this.saveMessages()
+      })
+    },
+    saveMessages:function(){
+      let myform = this.form
+      let name = myform.querySelector('.name').value
+      let content = myform.querySelector('.content').value
+      this.model.save(name,content)
+        .then(function (object) {
+          let li = document.createElement('li')
+          li.innerText = `${object.attributes.name}:${object.attributes.content}`
+          messageList.append(li)
+        })
+        .then( () => {
+          name = ''
+          content = ''
+        })
+    },
+
+
+  }
+
+  
+
+  
+  
+controller.init(view,model) 
+}.call()
